4382d0e35f814b3b499c5f0e9f300c6c9a8f8c62
QeMoo18 <82143441+QeMoo18@users.noreply.github.com>
2021-11-09 18:40:20 +0800

    progress report update

--- application/views/templates_report/body/progress_workstation.php
+++ application/views/templates_report/body/progress_workstation.php
@@ -17,54 +17,114 @@
 						}
 					?>
 					<div class="row">
-						<div class="col-md-2">
+						<div class="col-md-4">
 							<label>PPM Activity</label><br>
 							<select class="form-control" name="ppm_activity">
 								<option value=''>-- Select Activity --</option>
-								<!-- <option value="ALL">ALL</option> -->
 		          				<?= lookup_option_ppm_act_name('workstation'); ?>
 							</select>
 						</div>
 					</div>
 					<br>
 					<div class="row">
-						<div class="col-md-9"><label>Time Range</label></div>
-					</div>
-					<div class="row">
-						<div class="col-md-3">
-							<span class="glyphicon glyphicon-calendar" aria-hidden="true"></span>
-							<label>Start Date</label>
-							<input type="text" class="form-control datepicker" name="datestart" placeholder="Select Start Date...">
-						</div>
-						<div class="col-md-3">
-							<span class="glyphicon glyphicon-calendar" aria-hidden="true"></span>
-							<label>End Date</label>
-							<input type="text" class="form-control datepicker" name="dateend" placeholder="Select End Date...">
-						</div>
-					</div><br>
-					<div class="row">
 						<div class="col-md-2">
-							<label>Department</label><br>
-							<select class="form-control" name="department">
-								<option selected=""> -- Select Department -- </option>
-								<option value="ALL">ALL</option>
-								<?= lookup_department(); ?>
-							</select>
+						  <label>Select Type Report:</label>
 						</div>
-						<div class="col-md-2">
-							<label>Type Device</label><br>
-							<select class="form-control" name="ppm_device">
-								<option selected=""> -- Select Device -- </option>
-								<?= lookup_workstation_device_type(); ?>
-							</select>
-						</div>
-					</div><br>
-					<div class="row">
-						<div class="col-md-9" style="align-content: center;">
-							<button type="submit" class="btn btn-success">Generate Report</button>
-							<button type="button" class="btn btn-primary">Back</button>
+						<br>
+						<div class="col-md-4" style="align-content: center;">
+						  <input type="radio" name="typereport" onclick="handleClick(this);" value="daily" checked> Progress Report - Daily</input><br>
+						  <input type="radio" name="typereport" onclick="handleClick(this);" value="month"> Progress Report - Montly</input><br>
+						  <input type="radio" name="typereport" onclick="handleClick(this);" value="level"> Progress Report by Level</input>
 						</div>
 					</div>
+					<br>
+
+					<div class="row">
+						<div class="col-md-6">
+							<label>Report Title:</label>
+							<input type="text" class="form-control" name="title">
+						</div>
+					</div>
+					<br>
+
+					<div class="row">
+						<div class="col-md-3">
+						  <span class="glyphicon glyphicon-calendar" aria-hidden="true"></span>
+						  <label>Start Month</label>
+						  <input type="text" class="form-control datepicker" name="datestart" placeholder="Select Start Date...">
+						</div>
+						<div class="col-md-3">
+						  <span class="glyphicon glyphicon-calendar" aria-hidden="true"></span>
+						  <label>End Month</label>
+						  <input type="text" class="form-control datepicker" name="dateend" placeholder="Select End Date...">
+						</div>
+						<div class="col-md-3">
+						  <label id='level'>Level</label><br>
+						  <select class="form-control" name="level" id='level_select'>
+						  	<option selected=""> -- Select Level -- </option>
+							<option value="ALL">ALL</option>
+							<?= lookup_level(); ?>
+						  </select>
+						</div>
+						<div class="col-md-3">
+						  <label>Type Device</label><br>
+						  <select class="form-control" name="ppm_device">
+							<option selected=""> -- Select Device -- </option>
+							<?= lookup_workstation_device_type(); ?>
+						  </select>
+						</div>
+					</div>
+					<br>
+
+					<div class="row">
+						<div class="col-md-2">
+						  <label>Type Device</label><br>
+						  <select class="form-control" name="ppm_device">
+							<option selected=""> -- Select Device -- </option>
+							<option value="ALL">ALL</option>
+							<?= lookup_workstation_device_type(); ?>
+						  </select>
+						</div>
+					</div>
+					<br>
+
+					<div class="row">
+					  <div class="col-md-9" style="align-content: center;">
+					  	<button type="submit" class="btn btn-success">Generate Report</button>
+						<button type="button" class="btn btn-primary">Back</button>
+					  </div>
+					</div>
+
+					<!-- <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+	      			<script>
+	      			 function showRequiredOption(cval)
+	      			 {
+	      			  if(cval=='month')
+	      			   {
+	      			   	$('.level').show();
+	      			   }
+	      			  else if(cval=='level')
+	      			   {
+	      			   	$('.level').hide();
+	      			   }
+	      			  else
+	      			  {
+
+	      			  }
+	      			 }
+	      			</script> -->
+
+	      			<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+	      			<script>
+	      				$(document).ready(function(){
+	      					$("#hide").click(function(){
+	      						$("p").hide();
+	      					});
+	      					$("#show").click(function(){
+	      						$("p").show();
+	      					});
+	      				});
+	      			</script>
 				</form>
 			</div>
 		</div>
